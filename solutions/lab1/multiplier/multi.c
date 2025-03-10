f76e76e907d16b5c84cafdef81e6399a8fa0233e
Xenoneqq <realfatboimlg@gmail.com>
2025-03-10 10:43:52 +0100

    Created a detailed README file explaining the exercise

--- solutions/lab1/multiplier/multi.c
+++ solutions/lab1/multiplier/multi.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int main(void){
   int n1, n2;
-  printf("Prosze podac liczbe 1 : ");
+  printf("Input the first number : ");
   scanf("%d" , n1);
-  printf("Prosze podac liczbe 2 : ");
+  printf("Input the second number : ");
   scanf("%d" , n2);
   int res = n1 * n2;
-  printf("Wynik : %d" , res);
+  printf("Result : %d" , res);
   return 0;
 }
