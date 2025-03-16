b996e5b6e72de78628617831e1a566f3f4587801
Xenoneqq <realfatboimlg@gmail.com>
2025-03-16 16:07:58 +0100

    Finished all tasks

---
+++ solutions/lab2/instructions_pls_read.sh
@@ -0,0 +1,3 @@
+# run this command before 
+#running main with dynamic libraries
+export LD_LIBRARY_PATH=$(pwd):$LD_LIBRARY_PATH
--- solutions/lab2/main.c
+++ solutions/lab2/main.c
@@ -2,7 +2,12 @@
 #include "stdlib.h"
 #include "stdio.h"
 
+#ifdef USE_DYNAMIC
+#include <dlfcn.h>
+#endif
+
 int static_test(){
+  #ifndef USE_DYNAMIC
   int input;
   printf("Write number to test: ");
   scanf("%d", &input);
@@ -25,9 +30,61 @@ int static_test(){
 
   free(steps);
   return 0;
+  #else
+  printf("no static library found...");
+  #endif
+}
+
+int dynamic_test(){
+  #ifdef USE_DYNAMIC
+  void *handle = dlopen("libcollatz.so" , RTLD_LAZY);
+  if(!handle){
+    printf("error importing library...\n");
+    return -1;
+  }
+
+  int (*test_collatz_convergence)(int, int, int *);
+  test_collatz_convergence = dlsym(handle, "test_collatz_convergence");
+  if(dlerror() != NULL) {
+    printf("error importing function");
+    dlclose(handle);
+    return -1;
+  }
+
+  int input;
+  printf("Write number to test: ");
+  scanf("%d", &input);
+  int max_iter = 1000;
+  int *steps = malloc(max_iter * sizeof(int));
+  if(steps == NULL){
+    printf("memory allocation error...");
+    return -1;
+  }
+  int length = test_collatz_convergence(input, max_iter, steps);
+  
+  printf("Length is : %d\n", length);
+  if (length > 0) {
+    printf("Collatz sequence:\n");
+    for (int i = 0; i < length; i++) {
+        printf("%d ", steps[i]);
+    }
+    printf("\n");
+  }
+
+  free(steps);
+  dlclose(handle);
+  return 0;
+  #else
+  printf("No dynamic libraries!");
+  return -1;
+  #endif
 }
 
 int main(){
+  #ifdef USE_DYNAMIC
+  dynamic_test();
+  #else
   static_test();
+  #endif
   return 0;
 }
